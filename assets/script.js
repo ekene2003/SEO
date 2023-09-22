3d9a365594547b95334352f7d41a871d86cc1d22
Ekene <ekene2003@users.noreply.github.com>
2023-09-22 08:54:58 +0100

    i added new images

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+
 let open = document.querySelector(".open")
 let close = document.querySelector(".close")
 let navList = document.querySelector(".link-wrap")
@@ -12,3 +13,34 @@ close.onclick=()=>{
  open.style.display = "block";
  close.style.display = "none"
 }
+let heroImg = ["social media file 7.jpg","social media file 6.jpg","social media file 8.jpg"]
+let bg = document.querySelector(".flyers")
+i = -1
+// bg.innerHTML=`
+//   <img src="./assets/images/social media file 7.jpg" alt="" class="flyer-img">
+//   `
+setInterval(() => {
+ i++;
+  if (i==3) {
+   i=0
+  }
+  bg.innerHTML=`
+  <img src="./assets/images/${heroImg[i]}" alt="" class="flyer-img">
+  `
+ 
+
+}, 5000);
+
+let aboutImg = ["Social Media File 2.jpg","Social Media File 3.jpg","Social Media File 4.jpg"];
+let aboutWrap = document.querySelector(".about-left");
+b=0
+setInterval(() => {
+ b++;
+  if (b==3) {
+   b=0
+  }
+  aboutWrap.innerHTML=`
+  <img src="./assets/images/${aboutImg[i]}" alt="" class="flyer-img">
+  `
+
+}, 2000);
